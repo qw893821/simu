213ab5f2e227c9df79091abc0f8bc3a975f154cd
qw893821 <xiuman893821@gmail.com>
2018-03-20 12:27:37 -0400

    update
    bug:
    cold side will constantly drain heat from warm side. even when the temp is the opposite.

--- sketch.js
+++ sketch.js
@@ -9,12 +9,14 @@ let timer;
 let countTime;
 let img;
 let roombutton;
-const newMat={}
+const newMat = {}
 let textThickness;
 let textK;
-function preload(){
-    img=loadImage('./images/sample.jpg');
+
+function preload() {
+    img = loadImage('./images/sample.jpg');
 }
+
 function setup() {
     createCanvas(600, 700);
     heater = new Heater();
@@ -25,16 +27,16 @@ function setup() {
     timer = 0;
     countTime = 30;
     imageMode(CENTER);
-    image(img,width/2,height/2);
-    textThickness=createInput();
-    textThickness.position(width/2-200,150);
-    textK=createInput();
-    textK.position(width/2+200,150);
-    roombutton=createButton('Change Material');
-    roombutton.position(width/2,200);
+    image(img, width / 2, height / 2);
+    textThickness = createInput();
+    textThickness.position(width / 2 - 200, 150);
+    textK = createInput();
+    textK.position(width / 2 + 200, 150);
+    roombutton = createButton('Change Material');
+    roombutton.position(width / 2, 200);
     roombutton.mousePressed(setMat);
-    newMat.thickness=10;
-    newMat.k=401;
+    newMat.thickness = 10;
+    newMat.k = 401;
 }
 
 function draw() {
@@ -42,14 +44,12 @@ function draw() {
     Calculator();
 }
 
-
-
 function mouseClicked() {
 
 }
 
 function Calculator() {
-    if (timer >=countTime) {
+    if (timer >= countTime) {
         heater.changePower();
         cooler.changePower();
         heatMedia.captureHeat(heater.power);
@@ -60,13 +60,25 @@ function Calculator() {
         layer1.tempSide2 = coldMedia.temperature;
         coldMedia.captureHeat(layer1.innerTransfer());
         heatMedia.loseHeat(layer1.innerTransfer());
-        timer=0;
+        timer = 0;
     }
 }
 
-function setMat(){
-    newMat.thickness=Math.round(textThickness.value());
-    newMat.k=Math.round(textK.value());
-    layer1.changeMat={thickness:newMat.thickness,k:newMat.k};
-    
+function setMat() {
+    newMat.thickness = Number(textThickness.value());
+    newMat.k = Number(textK.value());
+    if (NumberCheck(newMat.thickness, newMat.k)) {
+        layer1.changeMat = {
+            thickness: newMat.thickness,
+            k: newMat.k
+        };
+    }
+    else{alert("Invalid Input Value");}
+}
+
+function NumberCheck(v1, v2) {
+    if ((typeof v1 != "number") || (typeof v2 != "number")||(v1 === NaN)||(v2 === NaN)||(v1 === 0) ) {
+        return false;
+    }
+    return true;
 }
